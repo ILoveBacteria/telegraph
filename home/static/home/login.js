df1ae3d9410bf11eca43fa7b9a5127aacd4324e3
Moein Arabi <moein.mo81@gmail.com>
2022-10-20 16:52:08 +0330

    Change function parameters

--- home/static/home/login.js
+++ home/static/home/login.js
@@ -1,7 +1,7 @@
-function submit_and_loading_button(button_id, form_id) {
+function submit_and_loading_button(button_id, form_id, text = "Loading...") {
     const element = document.getElementById(button_id);
-    element.setAttribute("disabled", "")
+    element.setAttribute("disabled", "");
     element.innerHTML = "<span class=\"spinner-border spinner-border-sm\" role=\"status\" aria-hidden=\"true\"></span>" +
-        " Loading..."
+        " " + text;
     document.getElementById(form_id).submit();
 }
