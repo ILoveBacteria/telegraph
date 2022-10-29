c21da055c6ff4d5d54a68de890192b865c0f89b0
Moein Arabi <moein.mo81@gmail.com>
2022-10-29 19:47:22 +0330

    Update the country code

--- home/static/home/login.js
+++ home/static/home/login.js
@@ -1,7 +1,12 @@
-function submit_and_loading_button(button_id, form_id, text = "Loading...") {
-    const element = document.getElementById(button_id);
+function submitAndLoadingButton(buttonId, formId, text = "Loading...") {
+    const element = document.getElementById(buttonId);
     element.setAttribute("disabled", "");
     element.innerHTML = "<span class=\"spinner-border spinner-border-sm\" role=\"status\" aria-hidden=\"true\"></span>" +
         " " + text;
-    document.getElementById(form_id).submit();
+    document.getElementById(formId).submit();
+}
+
+function updateCountryCode() {
+    const codeElement = document.getElementById("country_code");
+    codeElement.innerHTML = document.getElementById("id_country").value;
 }
